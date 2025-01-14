106226bfa58246988ab732a0caa4d81da326823b
Matvei-n <matveinacuk@gmail.com>
2025-01-14 11:22:28 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,19 @@
+#1
+username = "Нацук Матвей"
+#2
+title = "Grade 1. Этап 1: Задание 1"
+#3
+content = "Здесь содержится важная информация."
+#4
+status = "Не начата"
+#5
+created_date = "01-01-2025"
+#6
+issue_date = "31-01-2025"
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
