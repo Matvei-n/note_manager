7bfd5630aa53f698f95d8b3952e7d9b7dcb6ea5e
Matvei-n <matveinacuk@gmail.com>
2025-01-15 12:34:19 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,10 @@
+title = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+
+headers_list = {title, title2, title3 }
+
+print('Первый заголовок', title )
+print('Второй звголовок', title2 )
+print('Третий заголовок', title3 )
+print('Заголовки заметок в виде списка', headers_list)
